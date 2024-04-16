4c3db046efd7c52ad85b88052a8e411cf82f398e
Stefano Scarpa <scarpastefano@icloud.com>
2024-04-16 18:56:30 +0200

    :traccia 2

--- index.php
+++ index.php
@@ -6,21 +6,33 @@ $temp = 29.3;
 $name = "Stefano";
 $dev = true;
 
-var_dump($age);
-var_dump($temp);
-var_dump($name);
-var_dump($dev);
+// var_dump($age);
+// var_dump($temp);
+// var_dump($name);
+// var_dump($dev);
 
 const AGE = 30;
 const TEMP = 29.3;
 const NAME = "Stefano";
 const DEV = true;
 
-var_dump(AGE);
-var_dump(TEMP);
-var_dump(NAME);
-var_dump(DEV);
+// var_dump(AGE);
+// var_dump(TEMP);
+// var_dump(NAME);
+// var_dump(DEV);
 
+//traccia 2
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$text6 = "text2";
+$text7 = "bevuto";
+$text8 = "tutto";
+
+echo $text1 . " ". $text2 . " " . $text3 . $text4 . " " . $text2 . " " . $text7 . " " . $text8;
 
 
 
