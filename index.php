15936d5abd3f11df2c52e34db5bb0f83b854159c
Stefano Scarpa <scarpastefano@icloud.com>
2024-04-16 18:38:41 +0200

    :traccia 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+//traccia 1
+
+$age = 30;
+$temp = 29.3;
+$name = "Stefano";
+$dev = true;
+
+var_dump($age);
+var_dump($temp);
+var_dump($name);
+var_dump($dev);
+
+const AGE = 30;
+const TEMP = 29.3;
+const NAME = "Stefano";
+const DEV = true;
+
+var_dump(AGE);
+var_dump(TEMP);
+var_dump(NAME);
+var_dump(DEV);
+
+
+
+
+
+
+?>
